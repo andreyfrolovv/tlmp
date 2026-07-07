b45a961d77488fb14a6a059c6ee66d3e0a6a962f
afrolov <andrey.frolov.val@gmail.com>
2026-07-07 18:56:18 +0200

    update

--- src/tmplAiDetect.js
+++ src/tmplAiDetect.js
@@ -173,5 +173,7 @@ export default function renderAiAnalysis(responseData, fullText = null) {
     </div>
   `;
 
-  document.body.appendChild(container);
+  return container
+
+  //document.body.appendChild(container);
 }
